bb56528e53e2685448e517a000b8f87b324b7f63
mridha-rakib <rakibmahmudmridha@gmail.com>
2025-06-27 22:42:35 +0600

    files modified

--- src/app.js
+++ src/app.js
@@ -24,6 +24,8 @@ app.use(
   cors({
     origin: true,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   })
 );
 
