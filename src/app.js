38424ea779edc8684843d6fc22e4b362e0efaf56
mridha-rakib <rakibmahmudmridha@gmail.com>
2025-06-27 12:15:04 +0600

    [server]: app file modified

--- src/app.js
+++ src/app.js
@@ -13,12 +13,12 @@ import rootRoutes from "./routes/index.routes.js";
 const app = express();
 
 app.use(helmet());
-app.use(
-  cors({
-    origin: config.FRONTEND_ORIGIN,
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: config.FRONTEND_ORIGIN,
+//     credentials: true,
+//   })
+// );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
